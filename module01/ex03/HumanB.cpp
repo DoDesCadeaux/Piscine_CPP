8be03e112e70c1212501219e506cc878fba732dc
Dorian Duraku <dduraku@student.s19.be>
2023-08-31 15:24:33 +0200

    Protection if weapon is NULL

--- module01/ex03/HumanB.cpp
+++ module01/ex03/HumanB.cpp
@@ -5,10 +5,15 @@ HumanB::HumanB(std::string name) : _name(name) {return;}
 HumanB::~HumanB() {return;}
 
 void HumanB::attack() const {
-	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	if (this->_weapon == NULL) {
+		std::cout << this->_name << " doesn't have a weapon" << std::endl;
+	} else {
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	}
 
 	return;
 }
+
 
 void HumanB::setWeapon(Weapon& weapon) {
 	this->_weapon = &weapon;
