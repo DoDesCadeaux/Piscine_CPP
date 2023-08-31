36d4620a0cb12fe32f901797b1a4c0a2746be700
Dorian Duraku <dduraku@student.s19.be>
2023-08-31 15:06:43 +0200

    Protection mem. alloc

--- module01/ex00/newZombie.cpp
+++ module01/ex00/newZombie.cpp
@@ -1,7 +1,14 @@
 #include "Zombie.hpp"
 
-Zombie*		newZombie(std::string name) {
-	Zombie*	newZombie = new Zombie(name);
+Zombie* newZombie(std::string name) {
+	Zombie* newZombie = NULL;
+
+	try {
+		newZombie = new Zombie(name);
+	} catch (const std::bad_alloc& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return NULL;
+	}
 
 	return newZombie;
 }
--- module01/ex00/randomChump.cpp
+++ module01/ex00/randomChump.cpp
@@ -1,8 +1,15 @@
 #include "Zombie.hpp"
 
-void	randomChump(std::string name) {
-	Zombie* heapZombie = new Zombie(name);
+void randomChump(std::string name) {
+	Zombie* heapZombie = NULL;
+
+	try {
+		heapZombie = new Zombie(name);
+	} catch (const std::bad_alloc& e) {
+		std::cerr << "Error: " << e.what() << std::endl;
+		return;
+	}
 
 	heapZombie->announce();
-	delete(heapZombie);
+	delete heapZombie;
 }
