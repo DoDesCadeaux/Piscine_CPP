266d4d803eda3ffdb0ea50e7f76f438699359b14
DoDesCadeaux <dorian.duraku@hotmail.com>
2023-09-16 23:50:02 +0200

    finished

--- module04/ex00/main.cpp
+++ module04/ex00/main.cpp
@@ -6,15 +6,19 @@
 int main() {
 	const Animal* meta = new Animal();
 	const Animal* j = new Dog();
+	const Animal* k = new Cat();
 	const WrongAnimal* i = new WrongCat();
 	std::cout << j->getType() << " " << std::endl;
+	std::cout << k->getType() << " " << std::endl;
 	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
+	k->makeSound();
+	i->makeSound();
 	j->makeSound();
 	meta->makeSound();
 
 	delete meta;
 	delete j;
+	delete k;
 	delete i;
 	return 0;
 }
