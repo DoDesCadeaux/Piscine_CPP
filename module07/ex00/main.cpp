5d38e13c3edd89f366eacc09109ae3f72369eddf
Dorian Duraku <dduraku@student.s19.be>
2023-11-10 23:20:30 +0100

    subject main

--- module07/ex00/main.cpp
+++ module07/ex00/main.cpp
@@ -3,31 +3,17 @@
 #include "templates.hpp"
 
 int main(void) {
-	int a = 20;
-	int b = 40;
-
-	char c = 'c';
-	char d = 'd';
-
-	int mini = min<int>(a, b);
-	int maxi = max<int>(a, b);
-
-	float minif = min<float>(44.042f, 42.012f);
-	float maxif = max<float>(192.0223f, 31232.21233f);
-
-
-	std::cout << "before swap -> a : " << a << ", b : " << b << std::endl;
-	swap<int>(a, b);
-	std::cout << "after swap -> a : " << a << ", b : " << b << std::endl;
-
-	std::cout << "before swap -> c : " << c << ", d : " << d << std::endl;
-	swap<char>(c, d);
-	std::cout << "after swap -> c : " << c << ", d : " << d << std::endl;
-
-	std::cout << "min int : " << mini << std::endl;
-	std::cout << "max int : " << maxi << std::endl;
-	std::cout << "min float : " << minif << std::endl;
-	std::cout << "max float : " << maxif << std::endl;
-
-	return 0;
+		int a = 2;
+		int b = 3;
+		::swap( a, b );
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+		return 0;
 }
